decebf8b24538c9b42ba6b997c6d2953d949031e
Michael Janiszewski <mjaniszewski@godaddy.com>
2017-07-24 12:47:01 -0700

    Remove skrill update methods

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -2,35 +2,6 @@ module Spree
   CheckoutController.class_eval do
     before_filter :confirm_skrill, :only => [:update]
 
-    def skrill_return
-
-      unless @order.payments.where(:source_type => 'Spree::SkrillTransaction').present?
-        payment_method = PaymentMethod.find(params[:payment_method_id])
-        skrill_transaction = SkrillTransaction.new
-
-        payment = @order.payments.create({:amount => @order.total,
-                                         :source => skrill_transaction,
-                                         :payment_method => payment_method},
-                                         :without_protection => true)
-        payment.started_processing!
-        payment.pend!
-      end
-
-      @order.update_attributes({:state => "complete", :completed_at => Time.now}, :without_protection => true)
-
-      until @order.state == "complete"
-        if @order.next!
-          @order.update!
-          state_callback(:after)
-        end
-      end
-
-      @order.finalize!
-
-      flash.notice = Spree.t(:order_processed_successfully)
-      redirect_to completion_route
-    end
-
     def skrill_cancel
       flash[:error] = Spree.t(:payment_has_been_cancelled)
       redirect_to edit_order_path(@order)
--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,8 @@ Spree::Core::Engine.add_routes do
     resource :checkout, :controller => 'checkout' do
       member do
         get :skrill_cancel
-        get :skrill_return
       end
     end
   end
 
-  post '/skrill' => 'skrill_status#update'
 end
